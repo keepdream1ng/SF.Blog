6c1dbc342f70080fdd34973434ef05d895f1dd32
Anatolii Ivanov <gotnickname@gmail.com>
2023-12-27 20:36:44 +0530

    added comment aggregate with tests

---
+++ SF.Blog.Core/CommentAggregate/Comment.cs
@@ -0,0 +1,28 @@
+﻿using System.Data;
+
+namespace SF.Blog.Core;
+public class Comment : IDomainEntity
+{
+	public string Id { get; private set; }
+	public string OwnerId { get; private set; }
+	public string ReplyToId { get; private set; }
+	public DateTime Published { get; private set; }
+	public DateTime? Modified { get; private set; }
+	public string Text { get; private set; }
+
+    public Comment(string ownerId, string replyToId, string text)
+    {
+		OwnerId = Guard.Against.NullOrWhiteSpace(ownerId);
+		ReplyToId = Guard.Against.NullOrWhiteSpace(replyToId);
+		Text = Guard.Against.NullOrWhiteSpace(text);
+		Published = DateTime.Now;
+    }
+
+	// Internal methods below are designed to work with domain level services.
+
+	internal void Update(string text)
+	{
+		Text = Guard.Against.NullOrWhiteSpace(text);
+		Modified = DateTime.Now;
+	}
+}
---
+++ SF.Blog.UnitTests/Core/CommentTests.cs
@@ -0,0 +1,83 @@
+﻿namespace SF.Blog.UnitTests.Core;
+public class CommentTests
+{
+	[Fact]
+	public void Constructor_WithValidArguments_ShouldCreateComment()
+	{
+		// Arrange
+		string ownerId = "user1";
+		string replyToId = "parentCommentId";
+		string text = "This is a comment";
+
+		// Act
+		var comment = new Comment(ownerId, replyToId, text);
+
+		// Assert
+		Assert.NotNull(comment);
+		Assert.Equal(ownerId, comment.OwnerId);
+		Assert.Equal(replyToId, comment.ReplyToId);
+		Assert.Equal(text, comment.Text);
+		Assert.Equal(DateTime.Now.Date, comment.Published.Date); // Check date, ignoring time
+		Assert.Null(comment.Modified); // Modified should be null initially
+	}
+
+	[Theory]
+	[InlineData(" ", "replyToId", "Text")] // Whitespace ownerId
+	[InlineData("ownerId", " ", "Text")] // Whitespace replyToId
+	[InlineData("ownerId", "replyToId", " ")] // Whitespace text
+	[InlineData("", "replyToId", "Text")] // Empty ownerId
+	[InlineData("ownerId", "", "Text")] // Empty replyToId
+	[InlineData("ownerId", "replyToId", "")] // Empty text
+	public void Constructor_WithInvalidArguments_ShouldThrowArgumentException(string ownerId, string replyToId, string text)
+	{
+		// Act & Assert
+		Assert.Throws<ArgumentException>(() => new Comment(ownerId, replyToId, text));
+	}
+
+	[Theory]
+	[InlineData(null, "replyToId", "Text")] // Null ownerId
+	[InlineData("ownerId", null, "Text")] // Null replyToId
+	[InlineData("ownerId", "replyToId", null)] // Null text
+	public void Constructor_WithNullArguments_ShouldThrowArgumentNullException(string ownerId, string replyToId, string text)
+	{
+		// Act & Assert
+		Assert.Throws<ArgumentNullException>(() => new Comment(ownerId, replyToId, text));
+	}
+
+	[Fact]
+	public void Update_WithValidText_ShouldUpdateTextAndSetModified()
+	{
+		// Arrange
+		var comment = new Comment("ownerId", "replyToId", "Initial text");
+		string updatedText = "Updated text";
+
+		// Act
+		comment.Update(updatedText);
+
+		// Assert
+		Assert.Equal(updatedText, comment.Text);
+		Assert.NotNull(comment.Modified);
+	}
+
+	[Theory]
+	[InlineData(" ")] // Whitespace text
+	[InlineData("")] // Empty text
+	public void Update_WithInvalidText_ShouldThrowArgumentException(string text)
+	{
+		// Arrange
+		var comment = new Comment("ownerId", "replyToId", "Initial text");
+
+		// Act & Assert
+		Assert.Throws<ArgumentException>(() => comment.Update(text));
+	}
+
+	[Fact]
+	public void Update_WithNullText_ShouldThrowArgumentNullException()
+	{
+		// Arrange
+		var comment = new Comment("ownerId", "replyToId", "Initial text");
+
+		// Act & Assert
+		Assert.Throws<ArgumentNullException>(() => comment.Update(null));
+	}
+}
