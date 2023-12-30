da07673767675b3322acd4c5faccedef2397fa50
Anatolii Ivanov <gotnickname@gmail.com>
2023-12-30 15:35:51 +0530

    refactored AuthForManagerService

--- SF.Blog.Core/Services/AuthForManagerService.cs
+++ SF.Blog.Core/Services/AuthForManagerService.cs
@@ -7,7 +7,7 @@ public class AuthForManagerService
 
 	// Roles definition can be provided via configuration.
 	public AuthForManagerService(Role[] admins, Role[] moderators, IServiceProvider serviceProvider)
-    {
+	{
 		_adminAccessRoles = admins;
 		_moderatorAccessRoles = moderators;
 		_serviceProvider = serviceProvider;
@@ -15,42 +15,37 @@ public class AuthForManagerService
 
 	public PostManager GetManager(Post post, IUserAuth userAuth)
 	{
-		if (AuthForAccess(userAuth, post))
-		{
-			var repo = _serviceProvider.GetService(typeof(IRepository<Post>)) as IRepository<Post>;
-			return new PostManager(post, repo);
-		}
-		else
-			throw new UserAccessDeniedException();
-	}
-	public CommentManager GetManager(Comment comment, IUserAuth userAuth)
-	{
-		if (AuthForAccess(userAuth, comment))
-		{
-			var repo = _serviceProvider.GetService(typeof(IRepository<Comment>)) as IRepository<Comment>;
-			return new CommentManager(comment, repo);
-		}
-		else
-			throw new UserAccessDeniedException();
-	}
-	public UserManager GetManager(User user, IUserAuth userAuth)
-	{
-		if (AuthForAccess(userAuth, user, true))
-		{
-			var repo = _serviceProvider.GetService(typeof(IRepository<User>)) as IRepository<User>;
-			return new UserManager(user, repo);
-		}
-		else
-			throw new UserAccessDeniedException();
+		AuthForAccess(userAuth, post);
+		var repo = _serviceProvider.GetService(typeof(IRepository<Post>)) as IRepository<Post>;
+		return new PostManager(post, repo);
 	}
 
-    private bool AuthForAccess(IUserAuth user, IDomainEntity accessTarget, bool needAdminLevel = false)
+	public CommentManager GetManager(Comment comment, IUserAuth userAuth)
+	{
+		AuthForAccess(userAuth, comment);
+		var repo = _serviceProvider.GetService(typeof(IRepository<Comment>)) as IRepository<Comment>;
+		return new CommentManager(comment, repo);
+	}
+
+	public UserManager GetManager(User user, IUserAuth userAuth)
+	{
+		AuthForAccess(userAuth, user, true);
+		var repo = _serviceProvider.GetService(typeof(IRepository<User>)) as IRepository<User>;
+		return new UserManager(user, repo);
+	}
+
+	private bool AuthForAccess(IUserAuth user, IDomainEntity accessTarget, bool needAdminLevel = false)
 	{
 		// You have access to your own entities.
 		if (accessTarget.OwnerId == user.Id) return true;
 
 		// User roles are checked against known admin/moderator level roles.
 		Role[] rolesToCheck = needAdminLevel ? _adminAccessRoles : _moderatorAccessRoles;
-		return user.Roles.Intersect(rolesToCheck).Any();
+		bool result = user.Roles.Intersect(rolesToCheck).Any();
+		if (result is not true)
+		{
+			throw new UserAccessDeniedException();
+		}
+		return result;
 	}
 }
