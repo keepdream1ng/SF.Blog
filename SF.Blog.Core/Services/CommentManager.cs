2ee18e89efca48ea4089de82d1d5eac0d9bec30d
Anatolii Ivanov <gotnickname@gmail.com>
2023-12-30 11:54:55 +0530

    added comment manager

---
+++ SF.Blog.Core/Services/CommentManager.cs
@@ -0,0 +1,30 @@
+﻿namespace SF.Blog.Core;
+
+/// <summary>
+/// Manager class, what exposes to public internal <see cref="Comment"/> aggregate methods. Can be get after domain level access check via <see cref="AuthForManagerService"/>.
+/// </summary>
+public class CommentManager
+{
+	public Comment ManagedComment { get; private set; }
+	private readonly IRepository<Comment> _commentRepo;
+
+	// Constructor is internal so only domain services can create manager after checking user access.
+    internal CommentManager(Comment comment, IRepository<Comment> commentRepo)
+    {
+		ManagedComment = comment;
+		_commentRepo = commentRepo;
+	}
+
+	public async Task<Comment> UpdateCommentAsync(string newText)
+	{
+		ManagedComment.Update(newText);
+		await _commentRepo.UpdateAsync(ManagedComment);
+		return ManagedComment;
+	}
+
+	public async Task<bool> DeleteAsync()
+	{
+		await _commentRepo.DeleteAsync(ManagedComment);
+		return true;
+	}
+}
---
+++ SF.Blog.UnitTests/Core/CommentManagerTests.cs
@@ -0,0 +1,47 @@
+﻿using NSubstitute;
+
+namespace SF.Blog.UnitTests.Core;
+public class CommentManagerTests
+{
+	private Comment CreateNewComment()
+	{
+		var ownerId = "ownerId";
+		var replyToId = "replyToId";
+		var text = "Initial text";
+		var comment = new Comment(ownerId, replyToId, text);
+		return comment;
+	}
+
+	[Fact]
+	public async Task UpdateCommentAsync_Should_UpdateCommentAndRepository()
+	{
+		// Arrange
+		Comment comment = CreateNewComment();
+		var commentRepo = Substitute.For<IRepository<Comment>>();
+		var commentManager = new CommentManager(comment, commentRepo);
+		var newText = "Updated text";
+
+		// Act
+		await commentManager.UpdateCommentAsync(newText);
+
+		// Assert
+		Assert.Equal(newText, comment.Text);
+		await commentRepo.Received(1).UpdateAsync(Arg.Is<Comment>(c => c.Text == newText));
+	}
+
+	[Fact]
+	public async Task DeleteAsync_Should_DeleteCommentFromRepository()
+	{
+		// Arrange
+		Comment comment = CreateNewComment();
+		var commentRepo = Substitute.For<IRepository<Comment>>();
+		var commentManager = new CommentManager(comment, commentRepo);
+
+		// Act
+		var result = await commentManager.DeleteAsync();
+
+		// Assert
+		Assert.True(result);
+		await commentRepo.Received(1).DeleteAsync(comment);
+	}
+}
