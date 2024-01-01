ab9280253a4cb0116484174b9449f62c5a96e642
Anatolii Ivanov <gotnickname@gmail.com>
2024-01-01 14:12:22 +0530

    added interface for domain auth service

---
+++ SF.Blog.Core/Interfaces/IAuthForManagerService.cs
@@ -0,0 +1,8 @@
+﻿namespace SF.Blog.Core;
+
+public interface IAuthForManagerService
+{
+	CommentManager GetManager(Comment comment, IUserAuth userAuth);
+	PostManager GetManager(Post post, IUserAuth userAuth);
+	UserManager GetManager(User user, IUserAuth userAuth);
+}
