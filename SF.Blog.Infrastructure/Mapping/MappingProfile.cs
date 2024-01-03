865ef32e412933cc22b0c0e38104472cb34bbc3a
Anatolii Ivanov <gotnickname@gmail.com>
2024-01-03 11:55:09 +0530

    added mapping profile

---
+++ SF.Blog.Infrastructure/Mapping/MappingProfile.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using SF.Blog.Core;
+using SF.Blog.Infrastructure.Data.Models;
+
+namespace SF.Blog.Infrastructure.Mapping;
+public class MappingProfile : Profile
+{
+	public MappingProfile()
+	{
+		ShouldMapField = fieldInfo => true;
+		ShouldMapProperty = propertyInfo => true;
+		// From db model to domain entity.
+		CreateMap<CommentModel, Comment>();
+		CreateMap<TagModel, Tag>();
+		CreateMap<PostModel, Post>();
+		CreateMap<AppUserModel, User>();
+
+		// From domain entity to db model.
+		CreateMap<Comment, CommentModel>();
+		CreateMap<Tag, TagModel>();
+		CreateMap<Post, PostModel>();
+		CreateMap<User, AppUserModel>();
+	}
+}
