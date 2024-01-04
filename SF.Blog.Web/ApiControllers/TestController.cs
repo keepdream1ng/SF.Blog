c5e98631d30e5c49023010cc3272425dc24c14a0
Anatolii Ivanov <gotnickname@gmail.com>
2024-01-04 10:39:24 +0530

    added web project with api controllers

---
+++ SF.Blog.Web/ApiControllers/TestController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVCminimal.ApiControllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class TestController : ControllerBase
+{
+
+	[HttpGet]
+	public string Index()
+	{
+		return "Hello world again";
+	}
+}
