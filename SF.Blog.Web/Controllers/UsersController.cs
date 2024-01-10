8178b1df7088ab3c2762074db866041db0446d35
Anatolii Ivanov <gotnickname@gmail.com>
2024-01-10 14:14:18 +0530

    added user edit view

---
+++ SF.Blog.Web/Controllers/UsersController.cs
@@ -0,0 +1,83 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using SF.Blog.Infrastructure.Mediator.Commands;
+using SF.Blog.Web.Views.User;
+using Ardalis.Result;
+using SF.Blog.Core;
+using SF.Blog.Infrastructure.Mediator.Queries;
+using Microsoft.AspNetCore.Authorization;
+using SF.Blog.Web.Views.Users;
+using SF.Blog.UseCases.Users;
+
+namespace SF.Blog.Web.Controllers;
+
+public class UsersController (IMediator Mediator) : Controller
+{
+	[HttpGet]
+	public IActionResult Register()
+	{
+		return View("RegisterView");
+	}
+
+	[HttpPost]
+	[ValidateAntiForgeryToken]
+	public async Task<IActionResult> Register(RegisterViewModel model)
+	{
+		if (ModelState.IsValid)
+		{
+			Result<Core.User> result = await Mediator.Send(
+				new RegisterUserCommand(
+					model.EmailReg,
+					model.PasswordConfirm,
+					model.Name,
+					model.About,
+					model.DateOfBirth));
+			if (result.IsSuccess) return RedirectToAction("Index", "Home");
+			// Passing domain level validation, like "user exists" back to user.
+			result.Errors
+					.ToList()
+					.ForEach(e => ModelState.AddModelError(String.Empty, e));
+		}
+		return View("RegisterView", model);
+	}
+
+	[HttpGet]
+	[Authorize]
+	public async Task<IActionResult> Edit(string id)
+	{
+		if (string.IsNullOrEmpty(id))
+		{
+			Result<IUserAuth> authResult = await Mediator.Send(new GetIUserAuthByClaimsPricipalQuery(User));
+			id = authResult.Value.Id;
+		}
+		Result<User> userResult = await Mediator.Send(new GetUserByIdQuery(id));
+		if (!userResult.IsSuccess) return BadRequest();
+		return View("EditUserView", new EditUserViewModel(userResult.Value));
+	}
+
+	[HttpPost]
+	[Authorize]
+	public async Task<IActionResult> Edit(EditUserViewModel model)
+	{
+		if (ModelState.IsValid)
+		{
+			Result<IUserAuth> authResult = await Mediator.Send(new GetIUserAuthByClaimsPricipalQuery(User));
+			Result<Core.User> result = await Mediator.Send(
+				new UpdateUserCommand(
+					authResult.Value,
+					model.Id,
+					model.Name,
+					model.About,
+					model.DateOfBirth));
+			if (result.IsSuccess) return RedirectToAction("Index", "Home");
+			// Passing domain level validation back to user.
+			result.Errors
+					.ToList()
+					.ForEach(e => ModelState.AddModelError(String.Empty, e));
+		}
+
+		Result<User> userResult = await Mediator.Send(new GetUserByIdQuery(model.Id));
+		if (!userResult.IsSuccess) return BadRequest();
+		return View("EditUserView", new EditUserViewModel(userResult.Value));
+	}
+}
---
+++ SF.Blog.Web/Views/Users/EditUserView.cshtml.cs
@@ -0,0 +1,43 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using SF.Blog.Core;
+using System.ComponentModel.DataAnnotations;
+
+namespace SF.Blog.Web.Views.Users;
+public class EditUserViewModel
+{
+	public string Id { get; set; }
+
+	[Required]
+	[Display(Name = "Name")]
+	[Length(2, 25)]
+	public string Name { get; set; }
+
+	[Required]
+	[Display(Name = "About")]
+	[Length(2, 90)]
+	public string About { get; set; }
+
+	[Required]
+	[DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+	[Display(Name = "Date of birth")]
+	public DateTime DateOfBirth { get; set; } = DateTime.Now;
+	// Thanks to setting initial value it passes current and not 01/01/0001 date in the browser input.
+
+	public string Roles { get; set; }
+
+	public EditUserViewModel() { }
+	public EditUserViewModel(Core.User user)
+	{
+		Id = user.Id;
+		Name = user.Name;
+		About = user.About;
+		DateOfBirth = user.DateOfBirth;
+		Roles = String.Empty;
+		if (user.Roles.Count > 0)
+		{
+			string[] roles = user.Roles.Select(x => x.Name).ToArray();
+			Roles = String.Join(" ", roles);
+		}
+	}
+}
