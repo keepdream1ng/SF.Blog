ddf38efce102fc55c9d9fb0efb276e82c4b656a1
Anatolii Ivanov <gotnickname@gmail.com>
2024-01-15 17:55:52 +0530

    added posts by comments query

--- SF.Blog.Infrastructure/Mediator/Queries/GetCommentsByPost/GetCommentsByPostIdHandler.cs
+++ SF.Blog.Infrastructure/Mediator/Queries/GetCommentsByPost/GetCommentsByPostIdHandler.cs
@@ -4,13 +4,16 @@ using Microsoft.EntityFrameworkCore;
 using SF.Blog.Infrastructure.Data;
 using SF.Blog.Infrastructure.Data.DTO;
 
-namespace SF.Blog.Infrastructure.Mediator.Queries.GetAllComments;
-public class GetAllCommentsHandler (ApplicationDbContext DbContext) : IRequestHandler<GetAllCommentsQuery, Result<ICollection<CommentDTO>>>
+namespace SF.Blog.Infrastructure.Mediator.Queries;
+public class GetCommentsByPostIdHandler(
+	ApplicationDbContext DbContext
+	) : IRequestHandler<GetCommentsByPostIdQuery, Result<ICollection<CommentDTO>>>
 {
-	public Task<Result<ICollection<CommentDTO>>> Handle(GetAllCommentsQuery request, CancellationToken cancellationToken)
+	public Task<Result<ICollection<CommentDTO>>> Handle(GetCommentsByPostIdQuery request, CancellationToken cancellationToken)
 	{
 		ICollection<CommentDTO> comments = DbContext.Comments
 			.Include(c => c.Owner)
+			.Where(c => c.ReplyToId == request.Id)
 			.OrderBy(c => c.Published)
 			.Select(c => new CommentDTO(c.Id, c.ReplyToId, c.Owner.Name, c.Text))
 			.ToList();
