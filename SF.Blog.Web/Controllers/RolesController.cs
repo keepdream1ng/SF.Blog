180eeb2617f8c5b425d7a2f673c3b5215a51c965
Anatolii Ivanov <gotnickname@gmail.com>
2024-01-16 21:30:27 +0530

    added all roles view

---
+++ SF.Blog.Web/Controllers/RolesController.cs
@@ -0,0 +1,19 @@
+﻿using Ardalis.Result;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using SF.Blog.Infrastructure.Data.DTO;
+using SF.Blog.Infrastructure.Mediator.Queries;
+
+namespace SF.Blog.Web.Controllers;
+
+public class RolesController(IMediator Mediator) : Controller
+{
+	[HttpGet]
+	[Authorize(Roles = "Admin")]
+	public async Task<IActionResult> GetAll()
+	{
+        Result<ICollection<RoleDTO>> roleResult = await Mediator.Send(new GetAllRolesQuery());
+		return View("AllRolesView", roleResult.Value);
+	}
+}
