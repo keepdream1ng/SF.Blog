9a713384538e6ecb67d64be3740b8d27e7a60cb6
Anatolii Ivanov <gotnickname@gmail.com>
2024-01-21 12:14:41 +0530

    added global error handler

---
+++ SF.Blog.Web/Middleware/GlobalExceptionHandler.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Diagnostics;
+using SF.Blog.Core;
+
+namespace SF.Blog.Web.Middleware;
+
+public class GlobalExceptionHandler(
+    ILogger<GlobalExceptionHandler> logger,
+    IHttpContextAccessor httpContextAccessor
+    ) : IExceptionHandler
+{
+    public async ValueTask<bool> TryHandleAsync(HttpContext httpContext, Exception exception, CancellationToken cancellationToken)
+    {
+        logger.LogError(
+            "Error on request {@RequestPath}, {@Error}, {@DateTimeUtc}",
+            httpContext.Request.Path.Value,
+            exception.ToString(),
+            DateTime.UtcNow);
+
+        // Check if the request is for an API route
+        bool isApiRequest = httpContext.Request.Path.StartsWithSegments("/api");
+
+        if ( isApiRequest )
+        {
+            httpContext.Response.StatusCode = 500;
+            httpContext.Response.ContentType = "application/json";
+            await httpContext.Response.WriteAsJsonAsync(exception, cancellationToken);
+        }
+        else
+        {
+            httpContextAccessor.HttpContext.Response.Redirect("/Home/Oops?code=500");
+        }
+
+        return true;
+    }
+}
--- SF.Blog.Web/Program.cs
+++ SF.Blog.Web/Program.cs
@@ -11,75 +11,81 @@ namespace SF.Blog.Web;
 
 public static class Program
 {
-	public static void Main(string[] args)
-	{
-		var builder = WebApplication.CreateBuilder(args);
-		builder.ConfigureServices();
-		var app = builder.Build();
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+        builder.ConfigureServices();
+        var app = builder.Build();
 
-		// Configure the HTTP request pipeline.
-		if (!app.Environment.IsDevelopment())
-		{
-			app.UseExceptionHandler("/Home/Error");
-			app.UseHsts();
-		}
-		app.UseSwagger();
-		app.UseSwaggerUI();
-		app.UseHttpsRedirection();
-		app.UseStaticFiles();
+        // Configure the HTTP request pipeline.
+        if (!app.Environment.IsDevelopment())
+        {
+            app.UseExceptionHandler("/Home/Error");
+        }
+        else
+        {
+            app.UseExceptionHandler(_ => { });
+        }
+        app.UseHsts();
 
-		app.UseRouting();
+        app.UseSwagger();
+        app.UseSwaggerUI();
+        app.UseHttpsRedirection();
+        app.UseStaticFiles();
 
-		app.UseAuthentication();
-		app.UseAuthorization();
+        app.UseRouting();
 
-		// Using dotnet8 build in authentication api.
-		app.MapGroup("api/auth")
-			.MapIdentityApi<AppUserModel>();
+        app.UseAuthentication();
+        app.UseAuthorization();
 
-		app.MapControllerRoute(
-			name: "default",
-			pattern: "{controller=Home}/{action=Index}/{id?}");
+        // Using dotnet8 build in authentication api.
+        app.MapGroup("api/auth")
+            .MapIdentityApi<AppUserModel>();
+
+        app.MapControllerRoute(
+            name: "default",
+            pattern: "{controller=Home}/{action=Index}/{id?}");
 
         app.UseStatusCodePagesWithReExecute("/Home/Oops", "?code={0}");
 
-		app.Services.SeedDatabaseIfNeeded();
+        app.Services.SeedDatabaseIfNeeded();
 
-		NLog.LogManager.Setup()
-			.LoadConfigurationFromAppSettings();
+        NLog.LogManager.Setup()
+            .LoadConfigurationFromAppSettings();
 
-		app.Run();
-	}
+        app.Run();
+    }
 
-	private static void ConfigureServices(this WebApplicationBuilder builder)
-	{
-		var services = builder.Services;
-		// Core Services.
-		services.AddSingleton<IRoleConfigProvider, RoleConfigProvider>();
-		services.AddTransient<IAuthForManagerService, AuthForManagerService>();
-		// Use Cases Services.
-		services.AddUseCasesServices();
-		// Ifrastructure Services.
-		services.AddInfrastructureServices();
-		// Web Services.
-		services.AddControllersWithViews();
-		services.AddControllers(mvcOptions => mvcOptions.AddDefaultResultConvention());
-		services.AddEndpointsApiExplorer();
-		services.AddSwaggerGen();
-		services.AddIdentity<AppUserModel, IdentityRole>(options =>
-			{
-				options.SignIn.RequireConfirmedEmail = false;
-				options.Password.RequiredLength = 5;
-				options.Password.RequireNonAlphanumeric = false;
-				options.Password.RequireLowercase = false;
-				options.Password.RequireUppercase = false;
-				options.Password.RequireDigit = false;
-			})
-			.AddEntityFrameworkStores<ApplicationDbContext>()
-			.AddApiEndpoints();
+    private static void ConfigureServices(this WebApplicationBuilder builder)
+    {
+        var services = builder.Services;
+        // Core Services.
+        services.AddSingleton<IRoleConfigProvider, RoleConfigProvider>();
+        services.AddTransient<IAuthForManagerService, AuthForManagerService>();
+        // Use Cases Services.
+        services.AddUseCasesServices();
+        // Ifrastructure Services.
+        services.AddInfrastructureServices();
+        // Web Services.
+        services.AddExceptionHandler<Middleware.GlobalExceptionHandler>();
+        services.AddControllersWithViews();
+        services.AddControllers(mvcOptions => mvcOptions.AddDefaultResultConvention());
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen();
+        services.AddIdentity<AppUserModel, IdentityRole>(options =>
+            {
+                options.SignIn.RequireConfirmedEmail = false;
+                options.Password.RequiredLength = 5;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequireLowercase = false;
+                options.Password.RequireUppercase = false;
+                options.Password.RequireDigit = false;
+            })
+            .AddEntityFrameworkStores<ApplicationDbContext>()
+            .AddApiEndpoints();
 
-		// Logging.
-		builder.Logging.ClearProviders();
-		builder.Host.UseNLog();
-	}
+        // Logging.
+        builder.Logging.ClearProviders();
+        builder.Host.UseNLog();
+    }
 }
